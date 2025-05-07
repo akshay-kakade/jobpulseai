4121ce91b2b919cdb6a650843b6ad22afc07bb46
akshay-kakade <akshaykakade648@gmail.com>
2025-05-07 20:01:33 +0530

    update remove unnessesary icons

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import dayjs from 'dayjs'
 import Image from 'next/image';
-import { cn, getRandomInterviewCover } from '@/lib/utils';
+import { cn } from '@/lib/utils';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
@@ -16,7 +16,7 @@ const InterviewCard = async({interviewId, userId, role, type, techstack,
   ).format('DD MMM YYYY');
   console.log(userId)
   return (
-    <div className="card-border w-[360px] max-sm:w-full min-h-96">
+    <div className="card-border w-[360px] max-sm:w-full min-h-full">
     <div className="card-interview">
       <div>
         <div
@@ -27,13 +27,7 @@ const InterviewCard = async({interviewId, userId, role, type, techstack,
           <p className="badge-text ">{normalizedType}</p>
         </div>
 
-        <Image
-          src={getRandomInterviewCover()}
-          alt="cover-image"
-          width={90}
-          height={90}
-          className="rounded-full object-fit size-[90px]"
-        />
+        
         <h3 className="mt-5 capitalize">{role} Interview</h3>
 
         <div className="flex flex-row gap-5 mt-3">
